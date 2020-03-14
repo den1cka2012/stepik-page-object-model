c5db0e371e119c3b1d5282eca901203973c35621
den1cka2012@gmail.com <den1cka2012@gmail.com>
2020-03-14 22:04:28 +0200

    added product page test

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BTN_ADD_TO_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_NAME_BSKT = (By.CSS_SELECTOR, "#messages div:nth-child(1) strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRODUCT_PRICE_BSKT = (By.CSS_SELECTOR, "#messages div:nth-child(3) strong")
---
+++ pages/product_page.py
@@ -0,0 +1,33 @@
+import math
+from .base_page import BasePage
+from selenium.common.exceptions import NoAlertPresentException
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        self.product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        self.product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        btn_add_to_basket = self.browser.find_element(*ProductPageLocators.BTN_ADD_TO_BASKET)
+        btn_add_to_basket.click()
+
+    def check_name(self):
+        product_name_bskt = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_BSKT).text
+        assert self.product_name == product_name_bskt, f"{self.product_name} not equal {product_name_bskt}"
+
+    def check_price(self):
+        product_price_bskt = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_BSKT).text
+        assert self.product_price == product_price_bskt, f"{self.product_price} not equal {product_price_bskt}"
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
